4da6032180f2712bf22ccc00f44da1c33d688092
gbkgitatkp <ganeshbkarnuk11@gmail.com>
2019-01-14 16:48:07 +0530

    issue_1465 -copy.js

---
+++ issue_1465 - Copy.js
@@ -0,0 +1,14 @@
+start time 9.30am resolve time 11.30am
+Comment
+Hi Rahul Issue has been resolved.
+solution:- Need to check 'Generate Invoice' radio button.
+I have renewd the ip twice and in both cases invoice is generated.
+Please see the aatachments for the reference
+
+Solution:
+
+Hi Rahul 
+Issue1:- Visible 'Generate Invoice' Option on Renewals Page
+Solution:-'Configuration/System/Role Management'select Edit Permission and in this Select 'Invoice Generate' option under'Invoices' Menu.
+Issue2:- Set default Value of  'Generate Invoice' to 'yes'
+Solution:-Need to update the subscriber settings from'Configuration/Subscriber Setting' and select 'Generate Invoice for Admin Portal'
